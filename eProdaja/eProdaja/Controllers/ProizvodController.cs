f3fc1c640654b7906bf24defba55e3395cc05fb1
Zorka Kunovac <61622729+ZorkaKunovac@users.noreply.github.com>
2021-04-09 14:16:07 +0200

    Instaliranje dockera, upoznavanje sa http metodama/protokolima(trenutno GET)

---
+++ eProdaja/eProdaja/Controllers/ProizvodController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eProdaja.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProizvodController : ControllerBase
+    {
+        private static List<Proizvod> _proizvodi;
+
+        static ProizvodController()
+        {
+            _proizvodi = new List<Proizvod>()
+            {
+                new Proizvod()
+                {
+                    Id=1,
+                    Naziv="Laptop"
+                },
+                new Proizvod()
+                {
+                    Id=2,
+                    Naziv="Mis"
+                }
+            };
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Proizvod>  Get()
+        {
+            //List<Proizvod> list = new List<Proizvod>()
+            //{
+            //    new Proizvod()
+            //    {
+            //        Id=1,
+            //        Naziv="Laptop"
+            //    },
+            //    new Proizvod()
+            //    {
+            //        Id=2,
+            //        Naziv="Mis"
+            //    }
+            //};
+
+            return _proizvodi;
+        }
+
+        [HttpGet ("{id}")]
+        public Proizvod GetById(int id) {
+            return new Proizvod()
+            {
+                Id = id,
+                Naziv = $"Proizvod {id} "
+            };
+        }
+    }
+
+    public class Proizvod
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+    }
+}
+ 
