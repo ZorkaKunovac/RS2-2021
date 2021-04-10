5d1485efabc63e92dce6ebdc3fdd3b0d76ea412d
Zorka Kunovac <61622729+ZorkaKunovac@users.noreply.github.com>
2021-04-11 01:40:07 +0200

    Korisnici servis

---
+++ eProdaja/eProdaja/Controllers/KorisniciController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eProdaja.Database;
+using eProdaja.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eProdaja.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KorisniciController : Controller
+    {
+        private readonly IKorisniciService _korisniciService;
+
+        public KorisniciController(IKorisniciService korisniciService)
+        {
+            _korisniciService = korisniciService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Korisnici> Get()
+        {
+            return _korisniciService.Get();
+        }
+    }
+}
---
+++ eProdaja/eProdaja/Services/KorisniciService.cs
@@ -0,0 +1,21 @@
+﻿using eProdaja.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public class KorisniciService : IKorisniciService
+    {
+        public eProdajaContext Context { get; set; }
+        public KorisniciService(eProdajaContext context)
+        {
+            Context = context;
+        }
+        public List<Korisnici> Get()
+        {
+            return Context.Korisnicis.ToList();
+        }
+    }
+}
